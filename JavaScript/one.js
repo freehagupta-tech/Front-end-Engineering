f1192bd4b4828fa8287d5c0b3eb90669539bf578
freeha <freehagupta@gmail.com>
2026-07-28 14:38:05 +0530

    Initial commit

---
+++ JavaScript/one.js
@@ -0,0 +1,16 @@
+/*console.clear();
+let items=["item1","item2","item3","items4","items5"];
+let prices=[500,600,700,800,900];
+console.log("SrNo.\titem\tprice");
+for(let i=0;i<items.length;i++){
+    console.log((i+1)+"\t"+items[i]+"\t"+prices[i]);
+}
+const prompt=require('prompt-sync')();
+let srno=Number(prompt("Enter Sr No: "));
+let quantity=Number(prompt("Enter qunatity: "));
+let cost=quantity*prices[srno-1];
+console.log("Total cost is: ",cost);
+console.log();
+*/
+
+background blue and make a white box on half upward and then in that box there should be one circle and from between that cirlce two circles are forming and centre are same for them
---
+++ JavaScript/tempCodeRunnerFile.js
@@ -0,0 +1,8 @@
+console.clear();
+let items=["item1","item2","item3","items4","items5"];
+let prices=[500,600,700,800,900];
+console.log("SrNo.","\t","item","\t","price");
+for(let i=0;i<items.length;i++){
+    console.log((i+1),"\t",items[i],"\t",prices[i]);
+}
+console.log();
